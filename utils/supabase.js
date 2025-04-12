68bf6a9e77393069aa0f4cbce399a75fe3831b53
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-04-13 00:03:18 +0300

    utils

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 //создаем подключение к базе данных
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  'https://hlmifnolgghnjhmosohp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsbWlmbm9sZ2dobmpobW9zb2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MzA4MDUsImV4cCI6MjA1ODQwNjgwNX0.RC38TO1ciNhZmAZHBPZjj0pXeYzcnEEmnLX5DcviKxE'
 )
