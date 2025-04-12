4ba1884f0db7c9e4c3d3e28f7011121eabfb1119
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-04-12 22:33:46 +0300

    port 10000

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR file-storage-app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 3000
+EXPOSE 10000
 CMD ["npm", "run", "dev"]
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 "use client";
 import { Main } from "../../components/main";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from "./page.module.css"
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 export default function Home() {
   const router = useRouter();
  // const searchParams = useSearchParams();
